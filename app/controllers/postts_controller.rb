31280b0b0612671cc1b4f8ca502c357d00063370
Bhavya.Khanna <bhavya.khanna@kochartech.com>
2020-06-26 18:20:10 +0530

    changes

--- app/controllers/postts_controller.rb
+++ app/controllers/postts_controller.rb
@@ -1,10 +1,10 @@
-class PostsController < ApplicationController
+class PosttsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Postt.all
   end
 
   # GET /posts/1
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = Postt.new
   end
 
   # GET /posts/1/edit
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Postt.new(post_params)
 
     respond_to do |format|
       if @post.save
@@ -64,7 +64,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = Postt.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- db/migrate/20200331093759_create_postts.rb
+++ db/migrate/20200331093759_create_postts.rb
@@ -1,6 +1,6 @@
-class CreatePosts < ActiveRecord::Migration[6.0]
+class CreatePostts < ActiveRecord::Migration[6.0]
   def change
-    create_table :posts do |t|
+    create_table :postts do |t|
       t.string :title
       t.text :body
 
